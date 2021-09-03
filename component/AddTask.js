7e7c819cecfb7192f1216a86a3055b99110cacf2
Elisa Opalka <elisa.opalka@dexcom.com>
2021-09-03 13:48:46 -0400

    task component

---
+++ component/AddTask.js
@@ -0,0 +1,21 @@
+// const [text, setText] = useState('');
+// const changeHandler = val => {
+// setText(val)
+// }
+
+// <View>
+// <TextInput
+// style={styles.input}
+// placeholder="Add new task"
+// onChangeText={changeHandler}
+// />
+// </View>
+
+// const styles = StyleSheet.create({
+//     input: {
+//     margin: 10,
+//     paddingVertical: 6,
+//     borderBottomWidth: 1,
+//     borderBottomColor: 'grey'
+//     }
+//     })
